0a404f3516ff095550dd77f76a329c3b1f786e28
Fenil Dabhi <fenildabhi5857@gmail.com>
2023-01-19 19:03:11 +0530

    create new pages

---
+++ assets/js/loose-diamonds.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "initial";
+    } else {
+      x.style.display = "none";
+    }
+  }
